401d3ae465ae1f2721a2f0ed5922552c7763f80b
Matthew McCord <mattm.eternal@gmail.com>
2019-11-07 22:47:52 -0800

    initial acceleromater thingy

---
+++ target/1.4.2/photon/obj/src/user_module.o.d
@@ -0,0 +1,4 @@
+/Users/matthewmccord/Workspace/photon_iot/splendid_easter/target/1.4.2/photon//obj/src/user_module.o: \
+ src/user_module.c ../../../dynalib/inc/dynalib.h
+
+../../../dynalib/inc/dynalib.h:
